5c7a366e61ec00777b7e59857ad5d731133278ad
Olga Marchenko <enyalux@gmail.com>
2024-02-24 08:26:29 +0000

    Fix EC2 security groups properties

--- main.tf
+++ main.tf
@@ -56,7 +56,8 @@ resource "aws_vpc_security_group_ingress_rule" "sg_ec2_access" {
   from_port                = 80
   ip_protocol              = "tcp"
   to_port                  = 80
-  referenced_security_group_id = aws_security_group.sg_ec2_access.id
+#  referenced_security_group_id = aws_security_group.sg_ec2_access.id
+  cidr_ipv4                = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "ec2_internet_access" {
